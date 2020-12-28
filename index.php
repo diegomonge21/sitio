2b92dd02e830f257d5e81218836a29c83a3bd717
diegomonge21 <diegomonge21@gmail.com>
2020-12-28 11:32:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -241,16 +241,6 @@
     </section>
 		
     <footer class="ftco-footer ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">About</h2>
-              <p>I'm passionate for the audiovisual graphic world, in order to show attractive and creative messages.</p>
-              
-            </div>
-          </div>
-		</div>
 		
         <div class="row">
           <div class="col-md-12 text-center">
